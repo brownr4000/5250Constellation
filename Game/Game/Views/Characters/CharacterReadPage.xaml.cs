ea2068eeef1fbd14aeb72c373db73443c422a10a
Mtatineni05 <madhuri.tatineni22@yahoo.com>
2022-02-02 11:22:51 -0800

    Implementing updates to ProgressBars based on CharacterReadPage.xaml and cs

--- Game/Game/Views/Characters/CharacterReadPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterReadPage.xaml.cs
@@ -45,6 +45,12 @@ namespace Game.Views
             {
                 ClassValue.Text = result;
             }
+
+            // Setting Progress of named ProgressBars to the value of the
+            // related stored Attribute divided by maximum value
+            AttackProgressBar.Progress = ViewModel.Data.Attack / 9f;
+            DefenseProgressBar.Progress = ViewModel.Data.Defense / 9f;
+            SpeedProgressBar.Progress = ViewModel.Data.Speed / 9f;
         }
 
         /// <summary>
