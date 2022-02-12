d286c4e352b8ed1f74ea0274541526f60a409c6f
Robert Brown <rbrown3@seattleu.edu>
2022-02-11 23:24:17 -0800

    Updated CharacterModel data per game theme in DefaultData.cs

--- Game/Game/GameRules/DefaultData.cs
+++ Game/Game/GameRules/DefaultData.cs
@@ -303,10 +303,13 @@ namespace Game.GameRules
             var datalist = new List<CharacterModel>()
             {
                 new CharacterModel {
-                    Name = "Agent 1",
-                    Description = "The oldest Agent",
+                    Name = "Agent-01",
+                    Description = "Cybernetically enhanced with Alien technology",
                     Level = 1,
-                    MaxHealth = 5,
+                    MaxHealth = 10,
+                    Attack = 2,
+                    Defense = 6,
+                    Speed = 1,
                     ImageURI = "agent7.png",
                     Head = HeadString,
                     Necklass = NecklassString,
@@ -315,14 +318,17 @@ namespace Game.GameRules
                     Feet = FeetString,
                     RightFinger = RightFingerString,
                     LeftFinger = LeftFingerString,
-                    Job = CharacterJobEnum.Fighter
+                    Job = CharacterJobEnum.Defender
                 },
 
                 new CharacterModel {
-                    Name = "Agent Alpha",
-                    Description = "The first Agent",
+                    Name = "Agent Smith",
+                    Description = "A man in black",
                     Level = 1,
-                    MaxHealth = 5,
+                    MaxHealth = 10,
+                    Attack = 3,
+                    Defense = 5,
+                    Speed = 1,
                     ImageURI = "agent1.png",
                     Head = HeadString,
                     Necklass = NecklassString,
@@ -331,59 +337,69 @@ namespace Game.GameRules
                     Feet = FeetString,
                     RightFinger = RightFingerString,
                     LeftFinger = LeftFingerString,
-                    Job = CharacterJobEnum.Cleric
+                    Job = CharacterJobEnum.Defender
                 },
 
                 new CharacterModel {
-                    Name = "Agent Omega",
-                    Description = "The final defense",
+                    Name = "Slim Jim",
+                    Description = "He protecc, he attacc, he have bazooka on his bacc",
                     Level = 1,
                     MaxHealth = 8,
-                    Defense = 9,
+                    Attack = 3,
+                    Defense = 2,
+                    Speed = 4,
                     ImageURI = "agent2.png",
                     Head = HeadString,
-                    Necklass = NecklassString,
                     PrimaryHand = PrimaryHandString,
                     OffHand = OffHandString,
                     Feet = FeetString,
-                    RightFinger = RightFingerString,
-                    LeftFinger = LeftFingerString,
-                    Job = CharacterJobEnum.Fighter
+                    Job = CharacterJobEnum.Striker
                 },
 
                 new CharacterModel {
-                    Name = "Agent Dave",
-                    Description = "A normal guy",
-                    Level = 4,
-                    MaxHealth = 38,
+                    Name = "Prof. Tompkins",
+                    Description = "The Mirror Universe Mark Twain",
+                    Level = 1,
+                    MaxHealth = 7,
+                    Attack = 1,
+                    Defense = 3,
+                    Speed = 2,
                     ImageURI = "agent3.png",
-                    Job = CharacterJobEnum.Cleric
-                },
-
-                new CharacterModel {
-                    Name = "Agent K",
-                    Description = "Not Tommy Lee Jones",
-                    Level = 5,
-                    MaxHealth = 43,
-                    Attack = 2,
-                    ImageURI = "agent4.png",
-                    Job = CharacterJobEnum.Cleric
-                },
-
-                new CharacterModel {
-                    Name = "Agent Pug",
-                    Description = "Someone recruited a dog to become an Agent",
-                    Level = 5,
-                    MaxHealth = 21,
-                    ImageURI = "agent5.png",
                     Job = CharacterJobEnum.Support
                 },
 
                 new CharacterModel {
-                    Name = "Larry",
-                    Description = "No one understands how Larry got here",
+                    Name = "Agent Braxton",
+                    Description = "An actual baby",
                     Level = 1,
-                    MaxHealth = 3,
+                    MaxHealth = 8,
+                    Attack = 2,
+                    Defense = 2,
+                    Speed = 3,
+                    ImageURI = "agent4.png",
+                    Job = CharacterJobEnum.Striker
+                },
+
+                new CharacterModel {
+                    Name = "Agent Amelix",
+                    Description = "A deadly assasin from the near future",
+                    Level = 1,
+                    MaxHealth = 8,
+                    Attack = 3,
+                    Defense = 1,
+                    Speed = 5,
+                    ImageURI = "agent5.png",
+                    Job = CharacterJobEnum.Striker
+                },
+
+                new CharacterModel {
+                    Name = "Larry",
+                    Description = "From the Accounting department",
+                    Level = 1,
+                    MaxHealth = 6,
+                    Attack = 1,
+                    Defense = 3,
+                    Speed = 2,
                     ImageURI = "agent6.png",
                     Job = CharacterJobEnum.Support
                 }
