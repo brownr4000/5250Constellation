a12a1a86b91d2da4b4dcf24f11d17a12cd9f9687
Mtatineni05 <madhuri.tatineni22@yahoo.com>
2022-02-13 09:36:35 -0800

    Adding new fonts to App.xaml styles and AssemblyInfo.cs

--- Game/Game/AssemblyInfo.cs
+++ Game/Game/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("PlatNomor-WyVnn.ttf", Alias = "PlatNomor")]
+[assembly: ExportFont("SpaceSpeedFont.ttf", Alias = "SpaceSpeed")]
