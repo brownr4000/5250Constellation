1388a561d74afc103be5e3025562dfe287cc5b89
Han Duong <dinhhanduong@yahoo.com>
2022-02-16 15:57:11 -0800

    Added MonsterEnumExtensionsTests_Unknown_Default_Should_Pass() in MonsterEnumExtensionsTests.cs

---
+++ UnitTests/Models/Enum/MonsterEnumExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using Game.Models;
+using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class MonsterEnumExtensionsTests
+    {
+        [Test]
+        public void MonsterJobEnumExtensionsTests_Unknown_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterJobEnum.Unknown.ToMessage();
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Player", result);
+        }
+    }
+}
