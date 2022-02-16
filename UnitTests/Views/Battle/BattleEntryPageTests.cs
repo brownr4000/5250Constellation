0ec444aa1c01eecadcc823e0e2d88264715674bc
Han Duong <dinhhanduong@yahoo.com>
2022-02-16 12:40:54 -0800

    Added BattleEntryPage_Protect_Clicked_Default_Should_Pass() in BattleEntryPageTests.cs

--- UnitTests/Views/Battle/BattleEntryPageTests.cs
+++ UnitTests/Views/Battle/BattleEntryPageTests.cs
@@ -69,5 +69,20 @@ namespace UnitTests.Views
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void BattleEntryPage_Protect_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.ProtectButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+
+        }
+
     }
 }
