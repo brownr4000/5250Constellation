e273edcd53a369b0f5f510ca1ebe6bb66f041b23
Han Duong <dinhhanduong@yahoo.com>
2022-02-17 21:33:00 -0800

    Added MonsterJobEnumExtensionsTests_Brute_Default_Should_Pass() in MonstersEnumExtensionsTests.cs

--- UnitTests/Models/Enum/MonsterEnumExtensionsTests.cs
+++ UnitTests/Models/Enum/MonsterEnumExtensionsTests.cs
@@ -18,5 +18,44 @@ namespace UnitTests.Models
             // Assert
             Assert.AreEqual("Player", result);
         }
+
+        [Test]
+        public void MonsterJobEnumExtensionsTests_Brute_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = MonsterJobEnum.Brute.ToMessage();
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Brute", result);
+        }
+
+        //[Test]
+        //public void MonsterJobEnumExtensionsTests_Swift_Default_Should_Pass()
+        //{
+        //    // Arrange
+
+        //    // Act
+        //    var result = MonsterJobEnum.Swift.ToMessage();
+        //    // Reset
+
+        //    // Assert
+        //    Assert.AreEqual("Swift", result);
+        //}
+
+        //[Test]
+        //public void MonsterJobEnumExtensionsTests_Clever_Default_Should_Pass()
+        //{
+        //    // Arrange
+
+        //    // Act
+        //    var result = MonsterJobEnum.Clever.ToMessage();
+        //    // Reset
+
+        //    // Assert
+        //    Assert.AreEqual("Clever", result);
+        //}
     }
 }
