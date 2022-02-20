d6aa8cdcb3d7befc95630496d2ca4380245fdd82
Mtatineni05 <madhuri.tatineni22@yahoo.com>
2022-02-19 18:11:55 -0800

    Creating new page for CharacterAgentInfo.xaml and cs

---
+++ Game/Game/Views/Battle/CharacterAgentInfo.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.Models;
+using Game.ViewModels;
+using System.Linq;
+using Game.Views.Battle;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Selecting Characters for the Game
+    /// 
+    /// TODO: Team
+    /// Mike's game allows duplicate characters in a party (6 Mikes can all fight)
+    /// If you do not allow duplicates, change the code below
+    /// Instead of using the database list directly make a copy of it in the viewmodel
+    /// Then have on select of the database remove the character from that list and add to the part list
+    /// Have select from the party list remove it from the party list and add it to the database list
+    ///
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+    [DesignTimeVisible(false)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CharacterAgentInfoPage : ContentPage
+    {
+        private GenericViewModel<CharacterModel> ViewModel;
+
+        // Empty Constructor for UTs
+      //  public CharacterAgentInfoPage(bool UnitTest) { }
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the CharacterIndexView Model
+        /// </summary>
+        public CharacterAgentInfoPage()
+        {
+            InitializeComponent();
+
+            BindingContext = BattleEngineViewModel.Instance;
+
+            // Clear the Database List and the Party List to start
+            BattleEngineViewModel.Instance.PartyCharacterList.Clear();
+
+        }
+
+        /// <summary>
+        /// Constructor for Index Page accepting data
+        /// 
+        /// Get the CharacterIndexView Model
+        /// </summary>
+        public CharacterAgentInfoPage(GenericViewModel<CharacterModel> data)
+        {
+            InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+        }               
+    }
+}
