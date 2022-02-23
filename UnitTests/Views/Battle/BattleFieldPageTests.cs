8e01c5ed8974900eb16c5a3cefb219f52e66569a
Mtatineni05 <madhuri.tatineni22@yahoo.com>
2022-02-23 14:11:11 -0800

    Added unit tests in BattleFieldPageTests.cs

---
+++ UnitTests/Views/Battle/BattleFieldPageTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms;
+
+using Game;
+using Game.Views;
+using Game.Models;
+using Game.ViewModels;
+using Game.Views.Battle;
+
+namespace UnitTests.Views
+{
+    [TestFixture]
+    public class BattleFieldPageTests : BattleFieldPage
+    {
+        App app;
+
+        BattleFieldPage page;
+
+        public BattleFieldPageTests() : base(true) { }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new BattleFieldPage();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        [Test]
+        public void BattleFieldPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void BattleFieldPage_Fight_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.FightButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void BattleFieldPage_Defense_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.DefenseButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+    }
+}
