975fd1091387025434f17b24d28538d6b1e02ae8
Robert Brown <rbrown3@seattleu.edu>
2022-02-24 14:17:04 -0800

    Adding methods to BattleMessagesPage

--- Game/Game/Views/Battle/BattleMessagesPage.xaml.cs
+++ Game/Game/Views/Battle/BattleMessagesPage.xaml.cs
@@ -18,9 +18,109 @@ namespace Game.Views.Battle
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
     public partial class BattleMessagesPage : ContentPage
     {
+        // This uses the Instance so it can be shared with other Battle Pages as needed
+        public BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
+
         public BattleMessagesPage()
         {
             InitializeComponent();
+
+            // Draw the Characters
+            foreach (var data in EngineViewModel.Engine.EngineSettings.CharacterList)
+            {
+                PartyListFrame.Children.Add(CreatePlayerDisplayBox(data));
+            }
+
+            // Draw the Monsters
+            foreach (var data in EngineViewModel.Engine.EngineSettings.MonsterList)
+            {
+                MonsterListFrame.Children.Add(CreatePlayerDisplayBox(data));
+            }
+        }
+
+        /// <summary>
+        /// Return a stack layout with the Player information inside
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public StackLayout CreatePlayerDisplayBox(PlayerInfoModel data)
+        {
+            if (data == null)
+            {
+                data = new PlayerInfoModel();
+            }
+
+            // Hookup the image
+            var PlayerImage = new Image
+            {
+                Style = (Style)Application.Current.Resources["ImageBattleLargeStyle"],
+                Source = data.ImageURI
+            };
+
+            // Add the Level
+            var PlayerLevelLabel = new Label
+            {
+                Text = "Level : " + data.Level,
+                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                Padding = 0,
+                LineBreakMode = LineBreakMode.TailTruncation,
+                CharacterSpacing = 1,
+                LineHeight = 1,
+                MaxLines = 1,
+            };
+
+            // Add the HP
+            var PlayerHPLabel = new Label
+            {
+                Text = "HP : " + data.GetCurrentHealthTotal,
+                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                Padding = 0,
+                LineBreakMode = LineBreakMode.TailTruncation,
+                CharacterSpacing = 1,
+                LineHeight = 1,
+                MaxLines = 1,
+            };
+
+            var PlayerNameLabel = new Label()
+            {
+                Text = data.Name,
+                Style = (Style)Application.Current.Resources["ValueStyle"],
+                HorizontalOptions = LayoutOptions.Center,
+                HorizontalTextAlignment = TextAlignment.Center,
+                Padding = 0,
+                LineBreakMode = LineBreakMode.TailTruncation,
+                CharacterSpacing = 1,
+                LineHeight = 1,
+                MaxLines = 1,
+            };
+
+            var PlayerImageFrame = new Frame
+            {
+                BorderColor = (Color)Application.Current.Resources["ViewBackgroundColor"],
+                CornerRadius = 50,
+                Content = PlayerImage,
+            };
+
+            // Put the Image Button and Text inside a layout
+            var PlayerStack = new StackLayout
+            {
+                Style = (Style)Application.Current.Resources["PlayerInfoBox"],
+                HorizontalOptions = LayoutOptions.Center,
+                Padding = 0,
+                Spacing = 0,
+                Children = {
+                    PlayerImageFrame,
+                    PlayerNameLabel,
+                    PlayerLevelLabel,
+                    PlayerHPLabel,
+                },
+            };
+
+            return PlayerStack;
         }
     }
 }
