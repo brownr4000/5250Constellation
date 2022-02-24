ae2e6129b63121a53500fa9c98a2e93937d58087
Robert Brown <rbrown3@seattleu.edu>
2022-02-24 13:37:29 -0800

    Added initial BattleMessagesPage to project

---
+++ Game/Game/Views/Battle/BattleMessagesPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Game.Models;
+using Game.ViewModels;
+
+
+namespace Game.Views.Battle
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+    public partial class BattleMessagesPage : ContentPage
+    {
+        public BattleMessagesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
