6838e59dae4abde5220e74343d31975c1dc3f232
Mtatineni05 <madhuri.tatineni22@yahoo.com>
2022-02-27 10:56:34 -0800

    Adding Map grid with Characters and monsters in BattleFieldPage.xaml and cs

--- Game/Game/Views/Battle/BattleFieldPage.xaml.cs
+++ Game/Game/Views/Battle/BattleFieldPage.xaml.cs
@@ -19,6 +19,18 @@ namespace Game.Views
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
     public partial class BattleFieldPage : ContentPage
     {
+        // HTML Formatting for message output box
+        public HtmlWebViewSource htmlSource = new HtmlWebViewSource();
+
+        // Wait time before proceeding
+        public int WaitTime = 1500;
+
+        // Hold the Map Objects, for easy access to update them
+        public Dictionary<string, object> MapLocationObject = new Dictionary<string, object>();
+
+        // This uses the Instance so it can be shared with other Battle Pages as needed
+        public BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
+
         // Empty Constructor for UTs
         bool UnitTestSetting;
         public BattleFieldPage(bool UnitTest) { UnitTestSetting = UnitTest; }
@@ -29,17 +41,505 @@ namespace Game.Views
         public BattleFieldPage()
         {
             InitializeComponent();
+
+            BattleSequenceFrame.IsVisible = false;
+            NextMoveFrame.IsVisible = false;
+
+            // Set initial State to Starting
+            BattleEngineViewModel.Instance.Engine.EngineSettings.BattleStateEnum = BattleStateEnum.Starting;
+
+            // Set up the UI to Defaults
             BindingContext = BattleEngineViewModel.Instance;
+
+            // Create and Draw the Map
+            _ = InitializeMapGrid();
+
+            // Start the Battle Engine
+            _ = BattleEngineViewModel.Instance.Engine.StartBattle(false);
+
+            // Populate the UI Map
+            DrawMapGridInitialState();
+
+            // Ask the Game engine to select who goes first
+            _ = BattleEngineViewModel.Instance.Engine.Round.SetCurrentAttacker(null);
+
+            // Add Players to Display
+           // DrawGameAttackerDefenderBoard();
+
+            // Set the Battle Mode
+            //ShowBattleMode();
         }
 
-        public void FightButton_Clicked(object sender, EventArgs e)
+        /// <summary>
+        /// Dray the Player Boxes
+        /// </summary>
+        //public void DrawPlayerBoxes()
+        //{
+        //    var CharacterBoxList = CharacterBox.Children.ToList();
+        //    foreach (var data in CharacterBoxList)
+        //    {
+        //        _ = CharacterBox.Children.Remove(data);
+        //    }
+
+        //    // Draw the Characters
+        //    foreach (var data in BattleEngineViewModel.Instance.Engine.EngineSettings.PlayerList.Where(m => m.PlayerType == PlayerTypeEnum.Character).ToList())
+        //    {
+        //        CharacterBox.Children.Add(PlayerInfoDisplayBox(data));
+        //    }
+
+        //    var MonsterBoxList = MonsterBox.Children.ToList();
+        //    foreach (var data in MonsterBoxList)
+        //    {
+        //        _ = MonsterBox.Children.Remove(data);
+        //    }
+
+        //    // Draw the Monsters
+        //    foreach (var data in BattleEngineViewModel.Instance.Engine.EngineSettings.PlayerList.Where(m => m.PlayerType == PlayerTypeEnum.Monster).ToList())
+        //    {
+        //        MonsterBox.Children.Add(PlayerInfoDisplayBox(data));
+        //    }
+
+        //    // Add one black PlayerInfoDisplayBox to hold space in case the list is empty
+        //    CharacterBox.Children.Add(PlayerInfoDisplayBox(null));
+
+        //    // Add one black PlayerInfoDisplayBox to hold space incase the list is empty
+        //    MonsterBox.Children.Add(PlayerInfoDisplayBox(null));
+
+        //}
+
+        /// <summary>
+        /// Put the Player into a Display Box
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public StackLayout PlayerInfoDisplayBox(PlayerInfoModel data)
         {
-            // Fight button code
+            if (data == null)
+            {
+                data = new PlayerInfoModel
+                {
+                    ImageURI = ""
+                };
+            }
+
+            // Hookup the image
+            var PlayerImage = new Image
+            {
+                Style = (Style)Application.Current.Resources["ImageButtonBattleFieldWithBackgroundStyle"],
+                Source = data.ImageURI
+            };
+
+            // Put the Image Button and Text inside a layout
+            var PlayerStack = new StackLayout
+            {
+                Style = (Style)Application.Current.Resources["PlayerBattleDisplayBox"],
+                Children = {
+                    PlayerImage,
+                },
+            };
+
+            return PlayerStack;
         }
 
-        public void DefenseButton_Clicked(object sender, EventArgs e)
+        /// <summary>
+        /// Attack clicked method called
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void AttackButton_Clicked(object sender, EventArgs e)
         {
-            // Defense button code
+            // Attack button code
+        }
+
+        /// <summary>
+        /// Move clicked method called
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void MoveButton_Clicked(object sender, EventArgs e)
+        {
+            // Move button code
+        }
+
+        /// <summary>
+        /// Abilities clicked method called
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AbilitiesButton_Clicked(object sender, EventArgs e)
+        {
+            // Show abilities
+        }
+
+        /// <summary>
+        /// Create the Initial Map Grid
+        /// 
+        /// All lcoations are empty
+        /// </summary>
+        /// <returns></returns>
+        public bool InitializeMapGrid()
+        {
+            _ = BattleEngineViewModel.Instance.Engine.EngineSettings.MapModel.ClearMapGrid();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Draw the Map Grid
+        /// Add the Players to the Map
+        /// 
+        /// Need to have Players in the Engine first, to then put on the Map
+        /// 
+        /// The Map Objects are all created with the map background image first
+        /// 
+        /// Then the actual characters are added to the map
+        /// </summary>
+        public void DrawMapGridInitialState()
+        {
+            // Create the Map in the Game Engine
+            _ = BattleEngineViewModel.Instance.Engine.EngineSettings.MapModel.PopulateMapModel(BattleEngineViewModel.Instance.Engine.EngineSettings.PlayerList);
+
+            _ = CreateMapGridObjects();
+
+            _ = UpdateMapGrid();
+        }
+
+        /// <summary>
+        /// Walk the current grid
+        /// check each cell to see if it matches the engine map
+        /// Update only those that need change
+        /// </summary>
+        /// <returns></returns>
+        public bool UpdateMapGrid()
+        {
+            foreach (var data in BattleEngineViewModel.Instance.Engine.EngineSettings.MapModel.MapGridLocation)
+            {
+                // Use the ImageButton from the dictionary because that represents the player object
+                var MapObject = GetMapGridObject(GetDictionaryImageButtonName(data));
+                if (MapObject == null)
+                {
+                    return false;
+                }
+
+                var imageObject = (ImageButton)MapObject;
+
+                // Check automation ID on the Image, That should match the Player, if not a match, the cell is now different need to update
+                if (!imageObject.AutomationId.Equals(data.Player.Guid))
+                {
+                    // The Image is different, so need to re-create the Image Object and add it to the Stack
+                    // That way the correct monster is in the box.
+
+                    MapObject = GetMapGridObject(GetDictionaryStackName(data));
+                    if (MapObject == null)
+                    {
+                        return false;
+                    }
+
+                    var stackObject = (StackLayout)MapObject;
+
+                    // Remove the ImageButton
+                    stackObject.Children.RemoveAt(0);
+
+                    var PlayerImageButton = DetermineMapImageButton(data);
+
+                    stackObject.Children.Add(PlayerImageButton);
+
+                    // Update the Image in the Datastructure
+                    _ = MapGridObjectAddImage(PlayerImageButton, data);
+
+                    stackObject.BackgroundColor = DetermineMapBackgroundColor(data);
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Convert the Stack to a name for the dictionary to lookup
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public string GetDictionaryFrameName(MapModelLocation data)
+        {
+            return string.Format("MapR{0}C{1}Frame", data.Row, data.Column);
+        }
+
+        /// <summary>
+        /// Convert the Stack to a name for the dictionary to lookup
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public string GetDictionaryStackName(MapModelLocation data)
+        {
+            return string.Format("MapR{0}C{1}Stack", data.Row, data.Column);
+        }
+
+        /// <summary>
+        /// Covert the player map location to a name for the dictionary to lookup
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public string GetDictionaryImageButtonName(MapModelLocation data)
+        {
+            // Look up the Frame in the Dictionary
+            return string.Format("MapR{0}C{1}ImageButton", data.Row, data.Column);
+        }
+
+        /// <summary>
+        /// Populate the Map
+        /// 
+        /// For each map position in the Engine
+        /// Create a grid object to hold the Stack for that grid cell.
+        /// </summary>
+        /// <returns></returns>
+        public bool CreateMapGridObjects()
+        {
+            // Make a frame for each location on the map
+            // Populate it with a new Frame Object that is unique
+            // Then updating will be easier
+
+            foreach (var location in BattleEngineViewModel.Instance.Engine.EngineSettings.MapModel.MapGridLocation)
+            {
+                var data = MakeMapGridBox(location);
+
+                // Add the Box to the UI
+
+                MapGrid.Children.Add(data, location.Column, location.Row);
+            }
+
+            // Set the Height for the MapGrid based on the number of rows * the height of the BattleMapFrame
+
+            var height = BattleEngineViewModel.Instance.Engine.EngineSettings.MapModel.MapXAxiesCount * 60;
+
+            BattleMapDisplay.MinimumHeightRequest = height;
+            BattleMapDisplay.HeightRequest = height;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Get the Frame from the Dictionary
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public object GetMapGridObject(string name)
+        {
+            _ = MapLocationObject.TryGetValue(name, out var data);
+            return data;
+        }
+
+        /// <summary>
+        /// Make the Game Map Frame 
+        /// Place the Character or Monster on the frame
+        /// If empty, place Empty
+        /// </summary>
+        /// <param name="mapLocationModel"></param>
+        /// <returns></returns>
+        public Frame MakeMapGridBox(MapModelLocation mapLocationModel)
+        {
+            if (mapLocationModel.Player == null)
+            {
+                mapLocationModel.Player = BattleEngineViewModel.Instance.Engine.EngineSettings.MapModel.EmptySquare;
+            }
+
+            var PlayerImageButton = DetermineMapImageButton(mapLocationModel);
+
+            var PlayerStack = new StackLayout
+            {
+                Padding = 0,
+                Style = (Style)Application.Current.Resources["BattleMapImageBox"],
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                BackgroundColor = DetermineMapBackgroundColor(mapLocationModel),
+                Children = {
+                    PlayerImageButton
+                },
+            };
+
+            _ = MapGridObjectAddImage(PlayerImageButton, mapLocationModel);
+            _ = MapGridObjectAddStack(PlayerStack, mapLocationModel);
+
+            var MapFrame = new Frame
+            {
+                Style = (Style)Application.Current.Resources["BattleMapFrame"],
+                Content = PlayerStack,
+                AutomationId = GetDictionaryFrameName(mapLocationModel)
+            };
+
+            return MapFrame;
+        }
+
+        /// <summary>
+        /// This add the ImageButton to the stack to kep track of
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="MapModel"></param>
+        /// <returns></returns>
+        public bool MapGridObjectAddImage(ImageButton data, MapModelLocation MapModel)
+        {
+            var name = GetDictionaryImageButtonName(MapModel);
+
+            // First check to see if it has data, if so update rather than add
+            if (MapLocationObject.ContainsKey(name))
+            {
+                // Update it
+                MapLocationObject[name] = data;
+                return true;
+            }
+
+            MapLocationObject.Add(name, data);
+
+            return true;
+        }
+
+        /// <summary>
+        /// This adds the Stack into the Dictionary to keep track of
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="MapModel"></param>
+        /// <returns></returns>
+        public bool MapGridObjectAddStack(StackLayout data, MapModelLocation MapModel)
+        {
+            var name = GetDictionaryStackName(MapModel);
+
+            // First check to see if it has data, if so update rather than add
+            if (MapLocationObject.ContainsKey(name))
+            {
+                // Update it
+                MapLocationObject[name] = data;
+                return true;
+            }
+
+            MapLocationObject.Add(name, data);
+            return true;
+        }
+
+        /// <summary>
+        /// Set the Image onto the map
+        /// The Image represents the player
+        /// 
+        /// So a charcter is the character Image for that character
+        /// 
+        /// The Automation ID equals the guid for the player
+        /// This makes it easier to identify when checking the map to update thigns
+        /// 
+        /// The button action is set per the type, so Characters events are differnt than monster events
+        /// </summary>
+        /// <param name="MapLocationModel"></param>
+        /// <returns></returns>
+        public ImageButton DetermineMapImageButton(MapModelLocation MapLocationModel)
+        {
+            var data = new ImageButton
+            {
+                Style = (Style)Application.Current.Resources["BattleMapPlayerSmallStyle"],
+                Source = MapLocationModel.Player.ImageURI,
+
+                // Store the guid to identify this button
+                AutomationId = MapLocationModel.Player.Guid
+            };
+
+            switch (MapLocationModel.Player.PlayerType)
+            {
+                case PlayerTypeEnum.Character:
+                    data.Clicked += (sender, args) => SetSelectedCharacter(MapLocationModel);
+                    break;
+                case PlayerTypeEnum.Monster:
+                    data.Clicked += (sender, args) => SetSelectedMonster(MapLocationModel);
+                    break;
+                case PlayerTypeEnum.Unknown:
+                default:
+                    data.Clicked += (sender, args) => SetSelectedEmpty(MapLocationModel);
+
+                    // Use the blank cell
+                    data.Source = BattleEngineViewModel.Instance.Engine.EngineSettings.MapModel.EmptySquare.ImageURI;
+                    break;
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Set the Background color for the tile.
+        /// Monsters and Characters have different colors
+        /// Empty cells are transparent
+        /// </summary>
+        /// <param name="MapModel"></param>
+        /// <returns></returns>
+        public Color DetermineMapBackgroundColor(MapModelLocation MapModel)
+        {
+            string BattleMapBackgroundColor;
+            switch (MapModel.Player.PlayerType)
+            {
+                case PlayerTypeEnum.Character:
+                case PlayerTypeEnum.Monster:
+                case PlayerTypeEnum.Unknown:
+                default:
+                    BattleMapBackgroundColor = "BattleMapTransparentColor";
+                    break;
+            }
+
+            var result = (Color)Application.Current.Resources[BattleMapBackgroundColor];
+            return result;
+        }
+
+        /// <summary>
+        /// Event when an empty location is clicked on
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool SetSelectedEmpty(MapModelLocation data)
+        {
+            // TODO: Info
+
+            /*
+             * This gets called when the characters is clicked on
+             * Usefull if you want to select the location to move to etc.
+             * 
+             * For Mike's simple battle grammar there is no selection of action so I just return true
+             */
+
+            return true;
+        }
+
+        /// <summary>
+        /// Event when a Monster is clicked on
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool SetSelectedMonster(MapModelLocation data)
+        {
+            // TODO: Info
+
+            /*
+             * This gets called when the Monster is clicked on
+             * Usefull if you want to select the monster to attack etc.
+             * 
+             * For Mike's simple battle grammar there is no selection of action so I just return true
+             */
+
+            data.IsSelectedTarget = true;
+            NextMoveFrame.IsVisible = true;
+            return true;
+        }
+
+        /// <summary>
+        /// Event when a Character is clicked on
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public bool SetSelectedCharacter(MapModelLocation data)
+        {
+            // TODO: Info
+
+            /*
+             * This gets called when the characters is clicked on
+             * Usefull if you want to select the character and then set state or do something
+             * 
+             * For Mike's simple battle grammar there is no selection of action so I just return true
+             */
+
+            NextMoveFrame.IsVisible = true;
+            return true;
         }
     }
 }
