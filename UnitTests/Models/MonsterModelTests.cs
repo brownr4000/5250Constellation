4f9e26672a4409144d970c2e16b8bb649aa3e3a9
Han Duong <dinhhanduong@yahoo.com>
2022-03-02 19:03:42 -0800

    Fixed MonsterModel_Set_Default_Should_Pass() in MonsterModelTests.cs

--- UnitTests/Models/MonsterModelTests.cs
+++ UnitTests/Models/MonsterModelTests.cs
@@ -84,7 +84,7 @@ namespace UnitTests.Models
             Assert.IsNotNull(result.Id);
             Assert.AreEqual(result.Id, result.Guid);
 
-            Assert.AreEqual("Scorpio_Monster.png", result.ImageURI);
+            Assert.AreEqual("Musca_Monster.png", result.ImageURI);
             Assert.AreEqual(PlayerTypeEnum.Monster, result.PlayerType);
 
             Assert.AreEqual(true, result.Alive);
