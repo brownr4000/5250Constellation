9501391e51a6ff305621641fb73d24cfa0e5a1cb
Mtatineni05 <madhuri.tatineni22@yahoo.com>
2022-03-02 17:46:48 -0800

    Adding left and right arrows to pick random character in CharacterCreatePage.xaml and cs

--- Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
+++ Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
@@ -91,11 +91,54 @@ namespace Game.Views
         /// <param name="e"></param>
         public void RollDice_Clicked(object sender, EventArgs e)
         {
-            //_ = DiceAnimationHandeler();
-
-            //_ = RandomizeCharacter();
-
+            _ = RandomizeCharacter();
             return;
+        }
+
+        /// <summary>
+        /// 
+        /// Randomize the Character
+        /// Keep the Level the Same
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool RandomizeCharacter()
+        {
+            var levelIndex = LevelPicker.SelectedIndex;
+
+            // Need to change the Level picker to allow for the MaxHealth to be triggered
+            LevelPicker.SelectedIndex = 0;
+            LevelPicker.SelectedIndex = levelIndex;
+
+            ViewModel.Data.Level = levelIndex + 1;
+
+            ManageHealth();
+
+            // Randomize Name
+            ViewModel.Data.Name = RandomPlayerHelper.GetCharacterName();
+            ViewModel.Data.Description = RandomPlayerHelper.GetCharacterDescription();
+
+            // Randomize the Attributes
+            ViewModel.Data.Attack = RandomPlayerHelper.GetAbilityValue();
+            ViewModel.Data.Speed = RandomPlayerHelper.GetAbilityValue();
+            ViewModel.Data.Defense = RandomPlayerHelper.GetAbilityValue();
+
+            // Randomize an Item for Location
+            ViewModel.Data.Head = RandomPlayerHelper.GetItem(ItemLocationEnum.Head);
+            ViewModel.Data.Necklass = RandomPlayerHelper.GetItem(ItemLocationEnum.Necklass);
+            ViewModel.Data.PrimaryHand = RandomPlayerHelper.GetItem(ItemLocationEnum.PrimaryHand);
+            ViewModel.Data.OffHand = RandomPlayerHelper.GetItem(ItemLocationEnum.OffHand);
+            ViewModel.Data.RightFinger = RandomPlayerHelper.GetItem(ItemLocationEnum.Finger);
+            ViewModel.Data.LeftFinger = RandomPlayerHelper.GetItem(ItemLocationEnum.Finger);
+            ViewModel.Data.Feet = RandomPlayerHelper.GetItem(ItemLocationEnum.Feet);
+
+            ViewModel.Data.MaxHealth = RandomPlayerHelper.GetHealth(ViewModel.Data.Level);
+
+            ViewModel.Data.ImageURI = RandomPlayerHelper.GetCharacterImage();
+
+            _ = UpdatePageBindingContext();
+
+            return true;
         }
 
         /// <summary>
