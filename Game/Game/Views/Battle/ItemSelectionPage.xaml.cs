6bd47e237561859b72a1f6dfe5847f752ec74432
Han Duong <dinhhanduong@yahoo.com>
2022-03-02 21:37:53 -0800

    Commented out for reusability in ItemSelectionPage.xaml and ItemSelectPage.xaml.cs

--- Game/Game/Views/Battle/ItemSelectionPage.xaml.cs
+++ Game/Game/Views/Battle/ItemSelectionPage.xaml.cs
@@ -14,131 +14,131 @@ namespace Game.Views.Battle
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ItemSelectionPage : ContentPage
     {
-        // The item to select
-        public GenericViewModel<ItemModel> ViewModel = new GenericViewModel<ItemModel>();
+        //// The item to select
+        //public GenericViewModel<ItemModel> ViewModel = new GenericViewModel<ItemModel>();
 
         // The character
 
         public List<ItemModel> ItemList { get; }
 
-        // Item index variable, to load first item
-        public int itemImageIndex = 0;
+        //// Item index variable, to load first item
+        //public int itemImageIndex = 0;
 
-        //Hold a copy of data
-        public ItemModel DataCopy;
+        ////Hold a copy of data
+        //public ItemModel DataCopy;
 
         public ItemSelectionPage(List<ItemModel> itemList)
         {
             InitializeComponent();
 
-            this.ViewModel.Data = new ItemModel();
+            //this.ViewModel.Data = new ItemModel();
 
-            //Copy of Character to restore for cancel
-            DataCopy = this.ViewModel.Data;
+            ////Copy of Character to restore for cancel
+            //DataCopy = this.ViewModel.Data;
 
-            ItemList = itemList;
+            //ItemList = itemList;
 
-            // Load the first image in the list when the Create page is opened
-            this.ViewModel.Data.Name = ItemList[itemImageIndex].Name;
-            this.ViewModel.Data.ImageURI = ItemList[itemImageIndex].NewItemImageURI;
-            this.ViewModel.Data.Location = ItemList[itemImageIndex].Location;
-            this.ViewModel.Data.Range = ItemList[itemImageIndex].Range;
-            this.ViewModel.Data.Damage = ItemList[itemImageIndex].Damage;
-            this.ViewModel.Data.Attribute = ItemList[itemImageIndex].Attribute;
-            this.ViewModel.Data.Description = ItemList[itemImageIndex].Description;
+            //// Load the first image in the list when the Create page is opened
+            //this.ViewModel.Data.Name = ItemList[itemImageIndex].Name;
+            //this.ViewModel.Data.ImageURI = ItemList[itemImageIndex].NewItemImageURI;
+            //this.ViewModel.Data.Location = ItemList[itemImageIndex].Location;
+            //this.ViewModel.Data.Range = ItemList[itemImageIndex].Range;
+            //this.ViewModel.Data.Damage = ItemList[itemImageIndex].Damage;
+            //this.ViewModel.Data.Attribute = ItemList[itemImageIndex].Attribute;
+            //this.ViewModel.Data.Description = ItemList[itemImageIndex].Description;
         }
 
-        public bool UpdatePageBindingContext()
-        {
-            // Clear the Binding and reset it
-            BindingContext = null;
-            BindingContext = this.ViewModel;
+        //public bool UpdatePageBindingContext()
+        //{
+        //    // Clear the Binding and reset it
+        //    BindingContext = null;
+        //    BindingContext = this.ViewModel;
 
-            return true;
-        }
+        //    return true;
+        //}
 
-        public void LeftImageButton_Clicked(object sender, EventArgs e)
-        {
-            int imageCount = ItemList.Count;
+        //public void LeftImageButton_Clicked(object sender, EventArgs e)
+        //{
+        //    int imageCount = ItemList.Count;
 
-            // check if we are at the first photo and move to last photo when clicked
-            if (itemImageIndex == 0)
-            {
-                itemImageIndex = imageCount - 1;
-            }
+        //    // check if we are at the first photo and move to last photo when clicked
+        //    if (itemImageIndex == 0)
+        //    {
+        //        itemImageIndex = imageCount - 1;
+        //    }
 
-            // Move to the previous photo in the list
-            if (itemImageIndex > 0)
-            {
-                itemImageIndex--;
-            }
+        //    // Move to the previous photo in the list
+        //    if (itemImageIndex > 0)
+        //    {
+        //        itemImageIndex--;
+        //    }
 
-            // Update the image
-            this.ViewModel.Data.Name = ItemList[itemImageIndex].Name;
-            this.ViewModel.Data.ImageURI = ItemList[itemImageIndex].NewItemImageURI;
-            itemImage.Source = this.ViewModel.Data.ImageURI;
-            itemName.Text = this.ViewModel.Data.Name;
-            LocationLabel.Text = ItemList[itemImageIndex].Location.ToString();
-            RangeValue.Progress = ItemList[itemImageIndex].Range;
-            DamageValue.Progress = ItemList[itemImageIndex].Damage;
-            DamageLabel.Text = ItemList[itemImageIndex].Damage.ToString();
-            AttributesLabel.Text = ItemList[itemImageIndex].Attribute.ToString();
-            DescriptionLabel.Text = ItemList[itemImageIndex].Description;
-        }
+        //    // Update the image
+        //    this.ViewModel.Data.Name = ItemList[itemImageIndex].Name;
+        //    this.ViewModel.Data.ImageURI = ItemList[itemImageIndex].NewItemImageURI;
+        //    itemImage.Source = this.ViewModel.Data.ImageURI;
+        //    itemName.Text = this.ViewModel.Data.Name;
+        //    LocationLabel.Text = ItemList[itemImageIndex].Location.ToString();
+        //    RangeValue.Progress = ItemList[itemImageIndex].Range;
+        //    DamageValue.Progress = ItemList[itemImageIndex].Damage;
+        //    DamageLabel.Text = ItemList[itemImageIndex].Damage.ToString();
+        //    AttributesLabel.Text = ItemList[itemImageIndex].Attribute.ToString();
+        //    DescriptionLabel.Text = ItemList[itemImageIndex].Description;
+        //}
 
-        public void RightImageButton_Clicked(object sender, EventArgs e)
-        {
-            int imageCount = ItemList.Count;
+        //public void RightImageButton_Clicked(object sender, EventArgs e)
+        //{
+        //    int imageCount = ItemList.Count;
 
-            // check if we are at the last photo and move to first photo when clicked
-            if (itemImageIndex == imageCount - 1)
-            {
-                itemImageIndex = 0;
-            }
+        //    // check if we are at the last photo and move to first photo when clicked
+        //    if (itemImageIndex == imageCount - 1)
+        //    {
+        //        itemImageIndex = 0;
+        //    }
 
-            // Move to the next photo in the list
-            else if (itemImageIndex < imageCount - 1)
-            {
-                itemImageIndex++;
-            }
+        //    // Move to the next photo in the list
+        //    else if (itemImageIndex < imageCount - 1)
+        //    {
+        //        itemImageIndex++;
+        //    }
 
-            // Update the image
-            this.ViewModel.Data.Name = ItemList[itemImageIndex].Name;
-            this.ViewModel.Data.ImageURI = ItemList[itemImageIndex].NewItemImageURI;
-            itemImage.Source = this.ViewModel.Data.ImageURI;
-            itemName.Text = this.ViewModel.Data.Name;
-            LocationLabel.Text = ItemList[itemImageIndex].Location.ToString();
-            RangeValue.Progress = ItemList[itemImageIndex].Range;
-            DamageValue.Progress = ItemList[itemImageIndex].Damage;
-            DamageLabel.Text = ItemList[itemImageIndex].Damage.ToString();
-            AttributesLabel.Text = ItemList[itemImageIndex].Attribute.ToString();
-            DescriptionLabel.Text = ItemList[itemImageIndex].Description;
-        }
+        //    // Update the image
+        //    this.ViewModel.Data.Name = ItemList[itemImageIndex].Name;
+        //    this.ViewModel.Data.ImageURI = ItemList[itemImageIndex].NewItemImageURI;
+        //    itemImage.Source = this.ViewModel.Data.ImageURI;
+        //    itemName.Text = this.ViewModel.Data.Name;
+        //    LocationLabel.Text = ItemList[itemImageIndex].Location.ToString();
+        //    RangeValue.Progress = ItemList[itemImageIndex].Range;
+        //    DamageValue.Progress = ItemList[itemImageIndex].Damage;
+        //    DamageLabel.Text = ItemList[itemImageIndex].Damage.ToString();
+        //    AttributesLabel.Text = ItemList[itemImageIndex].Attribute.ToString();
+        //    DescriptionLabel.Text = ItemList[itemImageIndex].Description;
+        //}
 
-        //// <summary>
-        /// Cancel button clicked goes to index page without selecting  
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void Delete_Clicked(object sender, EventArgs e)
-        {
-            // Use the copy
-            ViewModel.Data.Update(DataCopy);
-            _ = await Navigation.PopModalAsync();
-        }
+        ////// <summary>
+        ///// Cancel button clicked goes to index page without selecting  
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //public async void Delete_Clicked(object sender, EventArgs e)
+        //{
+        //    // Use the copy
+        //    ViewModel.Data.Update(DataCopy);
+        //    _ = await Navigation.PopModalAsync();
+        //}
 
-        /// <summary>
-        /// Next button clicked selects the character and goes back to index page
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public async void Save_Clicked(object sender, EventArgs e)
-        {
-            MessagingCenter.Send(this, "Update", ViewModel.Data);
+        ///// <summary>
+        ///// Next button clicked selects the character and goes back to index page
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //public async void Save_Clicked(object sender, EventArgs e)
+        //{
+        //    MessagingCenter.Send(this, "Update", ViewModel.Data);
 
-            _ = await Navigation.PopModalAsync();
+        //    _ = await Navigation.PopModalAsync();
 
-        }
+        //}
 
     }
 }
