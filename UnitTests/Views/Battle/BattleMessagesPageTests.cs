9c084e494ad3702ebd72739c620790f46c5dfc0a
Han Duong <dinhhanduong@yahoo.com>
2022-03-07 23:45:58 -0800

    Added BattleMessagesPage_Constructor_Default_Should_Pass() in BattleMessagesPageTests.cs

--- UnitTests/Views/Battle/BattleMessagesPageTests.cs
+++ UnitTests/Views/Battle/BattleMessagesPageTests.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
 
 namespace Game.Views.Battle
 {
@@ -17,6 +18,9 @@ namespace Game.Views.Battle
         [SetUp]
         public void Setup()
         {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
             //This is your App.xaml and App.xaml.cs, which can have resources, etc.
             app = new App();
             Application.Current = app;
@@ -30,6 +34,20 @@ namespace Game.Views.Battle
         public void TearDown()
         {
             Application.Current = null;
+        }
+
+        [Test]
+        public void BattleMessagesPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+            var result = page;
+
+            // Act
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
         }
 
     }
