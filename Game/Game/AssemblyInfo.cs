629cf2e6d9eb6c39019f90de606a9322ac06d5ab
Mtatineni05 <madhuri.tatineni22@yahoo.com>
2022-03-09 09:51:30 -0800

    Adding new space quest font to App.xaml and AssemblyInfo.cs

--- Game/Game/AssemblyInfo.cs
+++ Game/Game/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using Xamarin.Forms;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("PlatNomor-WyVnn.ttf", Alias = "PlatNomor")]
 [assembly: ExportFont("SpaceSpeedFont.ttf", Alias = "SpaceSpeed")]
+[assembly: ExportFont("SpaceQuest-Xj4o.ttf", Alias = "SpaceQuest")]
