51b36ebf08176fa03b6d43f9e88a05fdfb6eb114
Han Duong <dinhhanduong@yahoo.com>
2022-03-10 23:18:12 -0800

    Added CharacterIndexPage_CollectionView_OnItemSelected_Valid_Should_Pass() in CharacterIndexPageTests.cs

--- UnitTests/Views/Characters/CharacterIndexPageTests.cs
+++ UnitTests/Views/Characters/CharacterIndexPageTests.cs
@@ -144,5 +144,24 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void CharacterIndexPage_CollectionView_OnItemSelected_Valid_Should_Pass()
+        {
+            // Arrange
+            var selectedCharacter = new CharacterModel();
+
+            CollectionView CharactersListView = (CollectionView)page.FindByName("CharactersListView");
+
+            // Act
+
+            // Triggers the OnCollectionViewSelectionChanged
+            CharactersListView.SelectedItem = selectedCharacter;
+
+            // Reset
+
+            // Arrange
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
