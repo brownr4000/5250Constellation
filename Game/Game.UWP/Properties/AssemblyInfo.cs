db624c05e3e5f633a44ea80a97cbae945a8f7b0f
Mtatineni05 <madhuri.tatineni22@yahoo.com>
2022-03-14 14:26:42 -0700

    Adding Assembly information in AssembleInfo.cs

--- Game/Game.UWP/Properties/AssemblyInfo.cs
+++ Game/Game.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Battle of Cosmos")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MIB vs Constellation Aliens")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Seattle University")]
-[assembly: AssemblyProduct("Game.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("SU")]
+[assembly: AssemblyProduct("Battle of Cosmos")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("SU")]
 [assembly: AssemblyCulture("")]
 
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: ComVisible(false)]
